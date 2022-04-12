52131aee881903b90933b55369f64f3a4eb498d1
rc-ops <rodrigocarvalhocontato@protonmail.com>
2022-04-12 10:56:06 -0300

    Adds TED 02-01 - WHILE

---
+++ aula04/While.java
@@ -0,0 +1,25 @@
+package com.aulasjava.universidade.aula04;
+
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+        int numero = (int) (Math.random() * 10);
+        Scanner sc = new Scanner(System.in);
+        int tentativas = 1;
+        while (true){
+            System.out.print("Informe um número de 0 a 10: ");
+            int escolha = sc.nextInt();
+            if (escolha > numero){
+                System.out.println("Errou! Informe um número menor.");
+                tentativas++;
+            } else if (escolha < numero) {
+                System.out.println("Errou! Informe um número maior");
+                tentativas++;
+            } else{
+                break;
+            }
+        }
+        System.out.println("Parabéns! Você acertou em " + tentativas + " tentativas");
+    }
+}
